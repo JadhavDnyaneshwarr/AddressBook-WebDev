db914adba5d976aa500e0d9f4731e23be4a257bd
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-03-14 14:53:12 +0530

    AddressBook htm/Css/Js

---
+++ js/app_home.js
@@ -0,0 +1,70 @@
+let contactList;
+window.addEventListener('DOMContentLoaded', (event) => {
+    contactList = getContactsFromStorage();
+    document.querySelector(".person-count").textContent = contactList.length;
+    console.log(contactList);
+    createInnerHtml();
+    localStorage.removeItem('editContact');
+});
+
+const getContactsFromStorage = () => {
+    return localStorage.getItem('AddressBookList') ? 
+                            JSON.parse(localStorage.getItem('AddressBookList')) : [];
+}
+
+const createInnerHtml = () => {
+    if(contactList.length == 0) return;
+    const headerHtml = "<th>Fullname</th><th>Address</th><th>City</th><th>State</th>"+
+                        "<th>Zip Code</th><th>Phone Number</th><th></th>";
+    let innerHtml = `${headerHtml}`;
+    // let contactList = createContactsJson();
+    for(const contact of contactList){
+        innerHtml = `${innerHtml}
+            <tr>
+                <td>${contact._name}</td>
+                <td>${contact._address}</td>
+                <td>${contact._city}</td>
+                <td>${contact._state}</td>
+                <td>${contact._zip}</td>
+                <td>${contact._phone}</td>
+                <td>
+                    <img id="${contact._id}" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="">
+                    <img id="${contact._id}" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="">
+                </td>
+            </tr>
+        `;
+    }
+    document.querySelector('#display').innerHTML = innerHtml;
+}
+
+const createContactsJson = () => {
+    let contactListLocal = [
+        {
+            _name: 'Jhanvi Kankhara',
+            _address: 'Vinayak Residency, B/h Sunshine School, Valakeshwari Nagari',
+            _city: 'Jamnagar',
+            _state: 'Gujarat',
+            _zip: '369369',
+            _phone: '7878709090'
+        }
+    ];
+    return contactListLocal;
+}
+
+const remove = (node) => {
+    let contact = contactList.find(contactData => contactData._id == node.id);
+    if(!contact) return;
+    const index = contactList.map(contactData => contactData._id)
+                                .indexOf(contact._id);
+    contactList.splice(index, 1);
+    localStorage.setItem("AddressBookList", JSON.stringify(contactList));
+    document.querySelector(".person-count").textContent = contactList.length;
+    createInnerHtml();
+}
+
+const update = (node) => {
+    let contact = contactList.find(contactData => contactData._id == node.id);
+    if(!contact) return;
+    localStorage.setItem('editContact', JSON.stringify(contact));
+    window.location.replace(site_properties.add_address_book_detail);
+}
---
+++ js/handleSelect.js
@@ -0,0 +1,16 @@
+var citiesByState = {
+    Gujarat: ["Choose City","Ahmedabad", "Jamnagar", "Rajkot", "Vadodara"],
+    Maharashtra: ["Choose City", "Mumbai", "Pune", "Nagpur", "Nashik"],
+    Rajasthan: ["Choose City","Jaipur", "Udaipur", "Bikaner", "Kota"],
+    Karnataka: ["Choose City","Banglore", "Hubli", "Mysore"]
+}
+function makeCitySubmenu(value) {
+    if (value.length == 0) document.getElementById("city").innerHTML = "<option></option>";
+    else {
+        var citiesOptions = "";
+        for (cityId in citiesByState[value]) {
+            citiesOptions += "<option>" + citiesByState[value][cityId] + "</option>";
+        }
+        document.getElementById("city").innerHTML = citiesOptions;
+    }
+}
